ed20811b911b3594b602920fbb838b2023ff38ca
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2022-06-29 19:01:26 +0100

    Notebook added

--- eval_mae.sh
+++ eval_mae.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python main_evaluate.py --model vit_large_patch16 \
+python main_evaluate.py --model vit_huge_patch14 \
     --data_path /mnt/sas/Datasets/ilsvrc12/ \
     --imagenet_c_path /mnt/sas/Datasets/imagenet_c_tar/complete/ \
-    --resume ./checkpoints/mae_finetuned_vit_large.pth \
+    --resume ./checkpoints/mae_finetuned_vit_huge.pth \
     --num_workers 8 --pin_mem
