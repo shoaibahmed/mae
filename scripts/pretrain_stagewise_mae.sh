f833347228c49065d9a106d144ab0e82459dee6f
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2022-08-20 23:49:50 +0200

    Stagewise training script updated

--- scripts/pretrain_stagewise_mae.sh
+++ scripts/pretrain_stagewise_mae.sh
@@ -17,15 +17,15 @@
 
 echo "Pretraining stagewise MAE"
 job=stagewise_mae_base_pretrain
-srun -p RTXA6000 -K -N1 --ntasks-per-node=8 --gpus-per-task=1 --cpus-per-gpu=8 --mem=400G \
-    --kill-on-bad-exit --job-name ${job} --nice=0 --time=24-00:00:00 \
+srun -p A100 -K -N1 --ntasks-per-node=8 --gpus-per-task=1 --cpus-per-gpu=8 --mem=400G \
+    --kill-on-bad-exit --job-name ${job} --nice=0 --time=3-00:00:00 \
     --container-mounts=/netscratch:/netscratch,/ds:/ds,/home/siddiqui:/home/siddiqui --container-image=/netscratch/enroot/nvcr.io_nvidia_pytorch_21.06-py3.sqsh \
     --container-workdir=`pwd` --container-mount-home --export="NCCL_SOCKET_IFNAME=bond,NCCL_IB_HCA=mlx5" \
     /opt/conda/bin/python main_pretrain_stagewise.py \
         --model stagewise_mae_vit_base_patch16 \
         --data_path /ds/images/imagenet/ \
         --batch_size 64 \
-        --accum_iter 2 \
+        --accum_iter 8 \
         --mask_ratio "0.2;0.4;0.6;0.75" \
         --epochs 800 \
         --warmup_epochs 40 \
